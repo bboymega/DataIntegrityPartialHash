124561fdf1bd90197bf05f98f8717637e3771529
megaboy <bboymatrixous@gmail.com>
2024-06-04 17:27:25 +0900

    updated

--- md5hashcalc.py
+++ md5hashcalc.py
@@ -1,11 +1,14 @@
 from Crypto.Hash import MD5
 import datetime
-file = open("/Volumes/RAMDISK/test.mp3", "rb")
-data = file.read()
-file.close()
-clock0 = datetime.datetime.now()
-md5_hash = MD5.new(data)
-md5_value = md5_hash.hexdigest()
-clock1 = datetime.datetime.now()
-print("Time elapsed with MD5 hashing:", clock1-clock0)
-print("Calculated MD5 Hash:", md5_value)
+class md5hashcalc:
+    def md5hashcalc(path):
+        file = open(path, "rb")
+        data = file.read()
+        file.close()
+        clock0 = datetime.datetime.now()
+        md5_hash = MD5.new(data)
+        md5_value = md5_hash.hexdigest()
+        clock1 = datetime.datetime.now()
+        print("Time elapsed with MD5 hashing:", clock1 - clock0)
+        print("Calculated MD5 Hash:", md5_value)
+        return 0
--- sender.py
+++ sender.py
@@ -3,6 +3,7 @@ from partialhash import partialhash
 import socket
 import pickle
 import datetime
+from md5hashcalc import md5hashcalc
 #from partialdataIO import partialdataIO
 file = open("/Volumes/RAMDISK/test.mp3", "rb")
 data = file.read()
@@ -20,6 +21,8 @@ clock0 = datetime.datetime.now()
 partial_param = partialhash.generatefinalhashquick(n, data, max_partial_size, data_size)
 clock1 = datetime.datetime.now()
 print("Time elapsed with partial hashing:", clock1-clock0)
+md5hashcalc.md5hashcalc("/Volumes/RAMDISK/test.mp3")
+
 partial_label = pickle.loads(partial_param[0])
 partial_data = pickle.loads(partial_param[1])
 instruction_tag = pickle.loads(partial_param[2])
