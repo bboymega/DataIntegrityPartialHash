ea8b6818168b633d7ec94db799109392f00f1dbb
megaboy <bboymatrixous@gmail.com>
2024-10-19 22:51:47 +0900

    updated

---
+++ loadaverage_md5.py
@@ -0,0 +1,24 @@
+import random
+from partialhash import partialhash
+from Crypto.Hash import MD5
+import argparse
+import os
+import time
+parser = argparse.ArgumentParser(description='Efficiency Test of Data Integrity Verification with Dynamic Partial Hash Algorithm.')
+parser.add_argument('file', type=str)
+args = parser.parse_args()
+file = open(args.file, "rb")
+data = file.read()
+file.close()
+data_size = len(data)
+n = random.randint(3 + int(data_size / 10485760),  5 + int(data_size / 10485760))  #Average block size for large files: 1MB
+max_partial_size = int(data_size / n * 2)
+print("Calculating Hash value of", file.name)
+clock0 = time.time()
+clock1 = clock0
+while clock1 - clock0 < 120:
+    md5_hash = MD5.new(data)
+    md5_value = md5_hash.hexdigest()
+    clock1 = time.time()
+load1, load5, load15 = os.getloadavg()
+print("MD5 - Load Average over the last 1 minute:",load1)
